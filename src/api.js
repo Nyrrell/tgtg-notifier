6bc34b87e9b0e77001bab99bed36cdb2f95c324b
Nyrrell <morel.bapt@gmail.com>
2021-12-09 16:27:32 +0100

    axios instance & default export

--- src/api.js
+++ src/api.js
@@ -1,30 +1,32 @@
 import { readFileSync } from 'fs';
-import axios from 'axios';
+import _axios from 'axios';
 
 const { locale } = JSON.parse(readFileSync('./config.json'));
 
-axios.defaults.baseURL = "https://apptoogoodtogo.com/api/";
-axios.defaults.withCredentials = true;
-axios.defaults.headers.common = {
-  "user-agent": "TGTG/21.9.3 Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus 5 Build/M4B30Z)",
-  "accept-language": locale,
-  "accept-encoding": "gzip;q=1.0, compress;q=0.5"
-};
+const axios = _axios.create({
+  baseURL: "https://apptoogoodtogo.com/api/",
+  withCredentials: true,
+  headers: {
+    "user-agent": "TGTG/21.9.3 Dalvik/2.1.0 (Linux; U; Android 6.0.1; Nexus 5 Build/M4B30Z)",
+    "accept-encoding": "gzip;q=1.0, compress;q=0.5",
+    "accept-language": locale
+  }
+});
 
 const itemEndpoint = "item/v7/";
 const authByEmail = "auth/v3/authByEmail";
-const signUpByEmail = "auth/v2/signUpByEmail";
+const signUpByEmail = "auth/v3/signUpByEmail";
 const refreshTokenEndpoint = "auth/v3/token/refresh";
 const authPollingEndpoint = "auth/v3/authByRequestPollingId";
 
-export const loginByEmail = async (email) => {
+const loginByEmail = async (email) => {
   return axios.post(authByEmail, {
     "device_type": "ANDROID",
     "email": email,
   });
 };
 
-export const authPolling = (email, pollingId) => {
+const authPolling = (email, pollingId) => {
   return axios.post(authPollingEndpoint, {
     "device_type": "ANDROID",
     "email": email,
@@ -32,7 +34,7 @@ export const authPolling = (email, pollingId) => {
   });
 };
 
-export const refreshToken = (accessToken, refreshToken) => {
+const refreshToken = (accessToken, refreshToken) => {
   return axios.post(refreshTokenEndpoint, {
       refresh_token: refreshToken,
     },
@@ -43,14 +45,14 @@ export const refreshToken = (accessToken, refreshToken) => {
     });
 };
 
-export const getItems = (accessToken, userId, favorite = true) => {
+const getItems = (accessToken, userId, favorite = true) => {
   return axios.post(itemEndpoint, {
       favorites_only: favorite,
       origin: {
         latitude: 0.0,
         longitude: 0.0,
       },
-      radius: 200,
+      radius: 20,
       user_id: userId,
     },
     {
@@ -59,3 +61,10 @@ export const getItems = (accessToken, userId, favorite = true) => {
       },
     });
 };
+
+export default {
+  loginByEmail,
+  authPolling,
+  refreshToken,
+  getItems
+}
