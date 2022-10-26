19b758584ddcadbb4fa66e26ff922fcced61150e
nyrrell <morel.bapt@gmail.com>
2022-10-26 10:13:01 +0200

    change keyv packages

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,7 @@
 import Keyv from "keyv";
-import KeyvSqlite from "@keyv/sqlite";
 
 class Database {
-    private database = new Keyv({ store: new KeyvSqlite({ uri: 'sqlite://database.sqlite' }) });
+    private database = new Keyv('sqlite://database.sqlite');
 
     set = (prefix: string, key: string, value: string) => this.database.set(`${prefix}:${key}`, value);
     get = (prefix: string, key: string) => this.database.get(`${prefix}:${key}`)
