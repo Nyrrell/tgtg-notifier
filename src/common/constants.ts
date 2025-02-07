e22a2f680a3bcef4ad271286ac8d16508cbd106b
nyrrell <morel.bapt@gmail.com>
2025-02-07 11:34:56 +0100

    add default constants

---
+++ src/common/constants.ts
@@ -0,0 +1,7 @@
+export const DEFAULT_APK_VERSION = '24.11.0';
+export const DEFAULT_AVAILABLE = 'Available';
+export const DEFAULT_PRICE = 'Price';
+export const DEFAULT_TIMEZONE = 'UTC';
+export const DEFAULT_LOCALE = 'en-US';
+export const DEFAULT_LOG_LEVEL = 'info';
+export const DEFAULT_CRON_SCHEDULE = '* * * * *';
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import { readFile } from 'node:fs/promises';
 import { exit } from 'node:process';
 
+import * as CONSTANT from './common/constants.js';
+
 const configFile = await readFile('./config.json', 'utf8').catch(({ message }) => console.error(message));
 
 if (!configFile) {
@@ -10,12 +12,12 @@ if (!configFile) {
 
 const config = JSON.parse(configFile);
 
-export const STOCK: string = config?.['language']?.['available'] || 'Available';
-export const PRICE: string = config?.['language']?.['price'] || 'Price';
-export const TIMEZONE: string = config['timezone'] || 'UTC';
-export const LOCALE: string = config['locale'] || 'en-US';
-export const LOG_LEVEL: string = config['logLevel'] || 'info';
+export const STOCK: string = config?.['language']?.['available'] || CONSTANT.DEFAULT_AVAILABLE;
+export const PRICE: string = config?.['language']?.['price'] || CONSTANT.DEFAULT_PRICE;
+export const TIMEZONE: string = config['timezone'] || CONSTANT.DEFAULT_TIMEZONE;
+export const LOCALE: string = config['locale'] || CONSTANT.DEFAULT_LOCALE;
+export const LOG_LEVEL: string = config['logLevel'] || CONSTANT.DEFAULT_LOG_LEVEL;
 export const ACCOUNTS: ACCOUNT[] = config['accounts'];
 export const TEST_NOTIFIERS: boolean = config?.['testNotifiers'];
 export const SEND_START_NOTIFICATION: string = config?.['sendStartNotification'] ?? true;
-export const CRON_SCHEDULE: string = config?.['cronSchedule'] || '* * * * *';
+export const CRON_SCHEDULE: string = config?.['cronSchedule'] || CONSTANT.DEFAULT_CRON_SCHEDULE;
