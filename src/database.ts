ac67a1a0825595ed7a13fc1a3e4a3c2afb213af4
nyrrell <morel.bapt@gmail.com>
2025-02-07 11:28:22 +0100

    update dependencies

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,12 @@
+import KeyvSqlite from '@keyv/sqlite';
 import Keyv from 'keyv';
 
 class Database {
-  private database = new Keyv('sqlite://database.sqlite', { ttl: 10000 * 60 });
+  private database = new Keyv(new KeyvSqlite('sqlite://database.sqlite'), { ttl: 10000 * 60 });
+
+  get getInstance(): Keyv<any> {
+    return this.database;
+  }
 
   set = (prefix: string, key: string, value: number, ttl?: number) => this.database.set(`${prefix}:${key}`, value, ttl);
   get = (prefix: string, key: string) => this.database.get(`${prefix}:${key}`);
